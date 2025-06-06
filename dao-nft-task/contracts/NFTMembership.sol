9c79ab0a96bb59d3137d5a83762653470f3a5fe5
markovic03 <markovicmare2003@gmail.com>
2025-06-06 15:21:07 +0200

    Update NFTMembership.sol

--- dao-nft-task/contracts/NFTMembership.sol
+++ dao-nft-task/contracts/NFTMembership.sol
@@ -5,20 +5,38 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFTMembership is ERC721URIStorage, Ownable {
-    uint256 public currentTokenId = 1;
-    uint256 public constant MINT_PRICE = 0.01 ether;
+    uint256 public tokenIdCounter;
+    uint256 public constant mintPrice = 0.01 ether;
     mapping(address => bool) public hasMinted;
 
-    constructor() ERC721("DAO Membership", "DAOM") {}
+    // Konstruktor prima adresu vlasnika (DAO ugovor)
+    constructor(address initialOwner) ERC721("DAO Membership NFT", "DAO") Ownable(initialOwner) {}
 
-    function mint(string memory tokenURI) external payable {
-        require(!hasMinted[msg.sender], "Only one NFT per address allowed");
-        require(msg.value == MINT_PRICE, "Mint price is 0.01 ETH");
+    // Funkcija za mintovanje prvog NFT-a od strane vlasnika bez placanja
+    function mintInitial(address to, string memory tokenURI) external onlyOwner {
+        require(tokenIdCounter == 0, "Initial NFT already minted");
 
-        _safeMint(msg.sender, currentTokenId);
-        _setTokenURI(currentTokenId, tokenURI);
+        tokenIdCounter++;
+        uint256 newTokenId = tokenIdCounter;
+
+        _mint(to, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        hasMinted[to] = true;
+    }
+
+    // Mintovanje za ostale korisnike, uz placanje mintPrice
+    function mintMembership(string memory tokenURI) public payable {
+        require(!hasMinted[msg.sender], "Already minted");
+        require(msg.value == mintPrice, "Wrong mint price");
+
+        tokenIdCounter++;
+        uint256 newTokenId = tokenIdCounter;
+
+        _mint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
         hasMinted[msg.sender] = true;
-        currentTokenId++;
     }
 }
 
